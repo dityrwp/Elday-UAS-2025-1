7587879d47c1e95e4813314f846d92b9395aac09
dityrwp <adityabaskoro1504@gmail.com>
2025-12-24 23:45:54 +0700

    added task 2

--- Task 2/params.m
+++ Task 2/params.m
@@ -1,10 +1,14 @@
-Vd_total = 400;      
-V_source = Vd_total/2;
+f_sw = 10000;
+f_fund = 50;
 R_load = 50;
+Vd_bus = 400;
+Vs = Vd_bus/2;
 
-ma = 1;
+ma = 0.8;
 V_carrier = 1;
 V_ref = ma * V_carrier;
 
-f_sw = 5000;
-f_fund = 50;
+L_f = 5.4e-3;
+R_f = 4.7e-6;
+
+
